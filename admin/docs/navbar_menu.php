3e6b2f96d0ed9b6446025b0aa47a7e6162b12f70
yudhacandra <channdra11@gmail.com>
2022-12-05 04:17:40 +0700

    update

--- admin/docs/navbar_menu.php
+++ admin/docs/navbar_menu.php
@@ -6,11 +6,6 @@
        
     
         <!-- User Menu-->
-        <li class="dropdown"><a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Open Profile Menu"><i class="fa fa-user-circle fa-2x" aria-hidden="true"></i>
-          <ul class="dropdown-menu settings-menu dropdown-menu-right">
-            <li><a class="dropdown-item" href="page-user.html"><i class="fa fa-user fa-lg"></i> Profile</a></li>
-            <li><a class="dropdown-item" href="../../logout.php"><i class="fa fa-sign-out fa-lg"></i> Logout</a></li>
-          </ul>
-        </li>
+        <a class="app-nav__item" href="../../logout.php"><i class="fa fa-sign-out fa-lg"></i> Logout</a>
       </ul>
     </header>
